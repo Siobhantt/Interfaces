7180df019420f0e272417d87647db031a05bd8f8
Siobhantt <luisa.cardozo@iesnervion.es>
2023-05-02 12:59:35 +0200

    Main Situacion

--- Interfaces/src/ej3/Situacion.java
+++ Interfaces/src/ej3/Situacion.java
@@ -3,6 +3,8 @@ package ej3;
 public class Situacion {
 
 	public static void main(String[] args) {
+		Polideportivo[] arrayPoli =  new Polideportivo[2];
+		EdificioOficinas[] arrayEdf = new EdificioOficinas[2];
 		
 
 	}
