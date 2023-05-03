a8b3a019a04be9bc715cdc8dc810a72ba2da792f
Siobhantt <luisa.cardozo@iesnervion.es>
2023-05-03 09:21:28 +0200

    Comentado ej2 de interfaces

--- Interfaces/src/ej2/Futbolista.java
+++ Interfaces/src/ej2/Futbolista.java
@@ -1,18 +1,42 @@
 package ej2;
-
+/**
+ * Clase que representa a un futbolista, implementa la interfaz comparable
+ * @author Luisa
+ *
+ */
 public class Futbolista implements Comparable{
+	/**
+	 * Atributo de numero de camiseta del jugador
+	 */
 private int numeroCamiseta;
+/**
+ * Nombre del jugador
+ */
 private String nombre;
+/**
+ * Edad del jugador
+ */
 private int edad;
+/**
+ * Goles anotados por el jugador
+ */
 private int numGoles;
-
+/**
+ * Constructor con parametros para la creacion del jugador
+ * @param numeroCamiseta
+ * @param nombre
+ * @param edad
+ * @param numGoles
+ */
 public Futbolista(int numeroCamiseta, String nombre, int edad, int numGoles) {
 	this.numeroCamiseta = numeroCamiseta;
 	this.nombre = nombre;
 	this.edad = edad;
 	this.numGoles = numGoles;
 }
-
+/**
+ * Metodo toString que muestra todas las caracteristicas del jugador
+ */
 @Override
 public String toString() {
 	String mensaje;
@@ -22,7 +46,10 @@ public String toString() {
 			+ "Numero de goles: " + numGoles;
 	return mensaje;
 }
-
+/**
+ * Metodo equals 
+ * Devuelve un booleano segun sean iguales el numero de camiseta y el nombre
+ */
 public boolean equals(Object obj) {
 	boolean igual=false;
 	Futbolista jugador = (Futbolista) obj; 
@@ -31,7 +58,9 @@ public boolean equals(Object obj) {
 	}
 	return igual;
 }//fin del metodo equals
-
+/**
+ * Compara el numero de camiseta y el nombre y segun el valor le da una posicion
+ */
 @Override
 public int compareTo(Object o) {
 	int result =0;
